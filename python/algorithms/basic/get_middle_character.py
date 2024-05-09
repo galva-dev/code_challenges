84cba67926468f22f16bdaff67da4dc53c5ff2b5
Alvaro Gonzales <alvarojuan2014@gmail.com>
2024-05-09 15:30:23 +0200

    Refactor get_middle_character.py for improved readability and adherence to PEP 8 style guide

--- python/algorithms/basic/get_middle_character.py
+++ python/algorithms/basic/get_middle_character.py
@@ -1,7 +1,8 @@
 def get_middle(s):
     if len(s) % 2 == 0:
-        return s[int(len(s)/2)-1:int(len(s)/2+1)]
+        return s[int(len(s) / 2) - 1 : int(len(s) / 2 + 1)]
     else:
-        return s[int(len(s)/2)]
+        return s[int(len(s) / 2)]
+
 
 print(get_middle("a"))
