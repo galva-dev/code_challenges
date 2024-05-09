7e4f204c3b56ddc3692ecb2a548bf1b537045dd5
Alvaro Gonzales <alvarojuan2014@gmail.com>
2024-05-09 15:30:09 +0200

    Refactor array_diff function to improve readability and adhere to PEP 8 style guide

--- python/algorithms/basic/array_diff.py
+++ python/algorithms/basic/array_diff.py
@@ -1,4 +1,5 @@
 def array_diff(a, b):
-    return(list(filter(lambda x:x not in b, a)))
+    return list(filter(lambda x: x not in b, a))
 
-print(array_diff([1,2,3,4], [1,2]))
+
+print(array_diff([1, 2, 3, 4], [1, 2]))
